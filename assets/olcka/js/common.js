baf52396d7457db75b00da9e0e287c0e3538d82b
Alex Pivovar <pivovar.zt@gmail.com>
2020-10-07 16:27:28 +0300

    olcka change

--- assets/olcka/js/common.js
+++ assets/olcka/js/common.js
@@ -4,3 +4,30 @@ $(document).ready(function(){
         return false;
     });
 });
+
+
+const btn1 = document.querySelector('#button1'),
+      btn2 = document.querySelector('#button2'),
+      btn3 = document.querySelector('#button3'),
+      divElement1 = document.querySelector('.section_5_item_2'),
+      divElement2 = document.querySelector('.section_5_item_3'),
+      divElement3 = document.querySelector('.section_5_item_4');
+
+btn1.addEventListener('click', () => {
+    divElement1.classList.toggle('show');
+    divElement2.classList.remove('show');
+    divElement3.classList.remove('show');
+});
+
+btn2.addEventListener('click', () => {
+    divElement2.classList.toggle('show');
+    divElement1.classList.remove('show');
+    divElement3.classList.remove('show');
+});
+
+btn3.addEventListener('click', () => {
+    divElement3.classList.toggle('show');
+    divElement1.classList.remove('show');
+    divElement2.classList.remove('show');
+});
+
